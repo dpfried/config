b09b7a617ab375b32fc0f705c34e5608bd4dd6a0
Daniel Fried <dfried@email.arizona.edu>
2012-06-24 11:31:42 -0700

    added speeddating, modified setup.sh to not fail on non-existing files

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd $DIR
 git pull && git submodule init && git submodule update && git submodule status
 
-rm ~/.emacs && ln -s $DIR/emacs ~/.emacs
-rm -rf ~/.oh-my-zsh && ln -s $DIR/oh-my-zsh ~/.oh-my-zsh
-rm -rf ~/.vim && ln -s $DIR/vim ~/.vim
-rm ~/.vimrc && ln -s $DIR/vimrc ~/.vimrc
-rm ~/.xmobarrc && ln -s $DIR/xmobarrc ~/.xmobarrc
-rm -rf ~/.xmonad && ln -s $DIR/xmonad ~/.xmonad
-rm ~/.zsh-aliases && ln -s $DIR/zsh-aliases ~/.zsh-aliases
-rm ~/.zshrc && ln -s $DIR/zshrc ~/.zshrc
-rm ~/.gitconfig && ln -s $DIR/gitconfig ~/.gitconfig
+rm ~/.emacs ; ln -s $DIR/emacs ~/.emacs
+rm -rf ~/.oh-my-zsh ; ln -s $DIR/oh-my-zsh ~/.oh-my-zsh
+rm -rf ~/.vim ; ln -s $DIR/vim ~/.vim
+rm ~/.vimrc ; ln -s $DIR/vimrc ~/.vimrc
+rm ~/.xmobarrc ; ln -s $DIR/xmobarrc ~/.xmobarrc
+rm -rf ~/.xmonad ; ln -s $DIR/xmonad ~/.xmonad
+rm ~/.zsh-aliases ; ln -s $DIR/zsh-aliases ~/.zsh-aliases
+rm ~/.zshrc ; ln -s $DIR/zshrc ~/.zshrc
+rm ~/.gitconfig ; ln -s $DIR/gitconfig ~/.gitconfig
